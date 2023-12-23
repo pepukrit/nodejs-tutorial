2e77ce214e43dd45989148113697154d50cf9c53
pepukrit <pepukrit@hotmail.com>
2023-12-23 15:40:21 +0700

    Added Stream tutorials

---
+++ 4-stream/1-read-write-stream.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync("./content/big-file.txt", "utf8");
+    // res.end(text);
+    const fileStream = fs.createReadStream("./content/big-file.txt", "utf8");
+    fileStream.on("open", () => {
+      fileStream.pipe(res);
+    });
+    fileStream.on("error", (err) => {
+      res.end(err);
+    });
+  })
+  .listen(5001);
